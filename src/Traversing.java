31ca5f4c8521a7342c91ae1d93af6fdd5b1ae7c2
bishops250 <biishops250@gmail.com>
2020-03-02 15:19:14 -0500

    Don't know to fix this.

--- src/Traversing.java
+++ src/Traversing.java
@@ -38,28 +38,35 @@ return userStates;
     }
 
     public static ArrayList<Integer> largestAndSmallest() throws IOException {
-        int largestNum=Integer.MIN_VALUE;
-        int indexOfSmallestNum;
-        int smallestNum=Integer.MAX_VALUE;
-        int indexOfLargestNum;
-        ArrayList<Integer> arrayDataStorage=new ArrayList<>();
+        Integer largestNum=Integer.MIN_VALUE;
+        int indexOfSmallestNum=0;
+        Integer smallestNum=Integer.MAX_VALUE;
+        int indexOfLargestNum=0;
+
         Scanner presetData= new Scanner(new File("src/numbers.txt"));
+        ArrayList<Integer> arrayDataStorage=new ArrayList<>();
 
         while (presetData.hasNextInt()) {
             arrayDataStorage.add(presetData.nextInt());
         }
-        for(int i:arrayDataStorage) {
-            if(i>smallestNum) {
-                smallestNum=i;
+        for(int i=0; i<arrayDataStorage.size();i++) {
+            if(arrayDataStorage.get(i)>smallestNum) {
+                smallestNum = arrayDataStorage.get(i);
+                indexOfSmallestNum=i;
             }
-             if(i<largestNum) {
-                largestNum=i;
+             if(arrayDataStorage.get(i)<largestNum) {
+                largestNum =arrayDataStorage.get(i);
+                indexOfLargestNum=i;
             }
 
         }
 
+        arrayDataStorage.remove(indexOfSmallestNum);
         arrayDataStorage.add(0,smallestNum);
+
+        arrayDataStorage.remove(indexOfLargestNum+1);
         arrayDataStorage.add(arrayDataStorage.size(),largestNum);
+
 
         return arrayDataStorage;
     }
